76bd3f927bb6552e10dbb415e18a85904d8fbcc4
Alaina Hu <alaina.hu@mail.utoronto.ca>
2024-01-22 21:08:07 +0000

    Edited paper.qmd

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -33,6 +33,17 @@ cleaned_ward_data <- as.data.frame(t(cleaned_ward_data)) |>
 cleaned_ward_data$WARD = 1:25
 cleaned_ward_data = cleaned_ward_data[c("WARD", setdiff(names(cleaned_ward_data),
                                                         "WARD"))]
+
+ward_names <- c("Etobicoke North", "Etobicoke Centre", "Etobicoke-Lakeshore", 
+                "Parkdale-High Park","York South-Weston", "York Centre", 
+                "Humber River-Black Creek", "Eglinton-Lawrence",
+                "Davenport", "Spadina-Fort York", "University-Rosedale",
+                "Toronto-St. Paul's", "Toronto Centre", "Toronto-Danforth",
+                "Don Valley West", "Don Valley East", "Don Valley North",
+                "Willowdale", "Beaches-East York", "Scarborough Southwest",
+                "Scarborough Centre", "Scarborough-Agincourt",
+                "Scarborough North", "Scarborough-Guildwood",
+                "Scarborough-Rouge Park")
 count_by_ward <- cleaned_art_data |> group_by(WARD) |> count(WARD)
 analysis_data <- left_join(cleaned_ward_data, count_by_ward)  
 analysis_data$n <- replace(analysis_data$n, is.na(analysis_data$n), 0)
@@ -42,7 +53,9 @@ analysis_data$WARD <- as.character(analysis_data$WARD)
 analysis_data$population <- as.numeric(analysis_data$population)
 analysis_data$minority_population <- as.numeric(analysis_data$minority_population)
 analysis_data$income <- as.numeric(analysis_data$income)
-
+analysis_data$ward_name <- ward_names
 
 #### Save data ####
-write_csv(analysis_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_art_data, "/cloud/project/outputs/data/cleaned_art_data.csv")
+write_csv(cleaned_ward_data, "/cloud/project/outputs/data/cleaned_ward_data.csv")
+write_csv(analysis_data, "/cloud/project/outputs/data/analysis_data.csv")
